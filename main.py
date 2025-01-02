3892deee51dac8b0b8de6c5079197957d53a072f
54CHA-PJ <sachacruzabsi@gmail.com>
2025-01-02 19:37:24 +0100

    .

--- main.py
+++ main.py
@@ -121,41 +121,44 @@ def main():
             print("Could not find 'Month' in the image.")
             return
         
-        # Delete planning.ics in downloads folder if it already exists
-        planning_ics_path = os.path.join(DOWNLOADS_PATH, "planning.ics")
-        if os.path.exists(planning_ics_path):
-            os.remove(planning_ics_path)
-            print("Deleted older planning.ics file.")        
-        
-        # Click 4: Download the planning.ics file
-        download_icon_locator = (By.XPATH, "//i[contains(@class, 'fa fa-download Fs20 Gray')]")
-        WebDriverWait(driver, 30).until(EC.visibility_of_element_located(download_icon_locator))
-        print("Download icon is now visible.")
+        for k in range(4):
+            print(f"\n --- Month {k+1} --- \n")
 
-        # Locate and click the download icon using JavaScript
-        download_icon = driver.find_element(By.XPATH, "//i[contains(@class, 'fa fa-download Fs20 Gray')]")
-        driver.execute_script("arguments[0].click();", download_icon)
-        print("Clicked on the download icon using JavaScript.")
-        
-        # Wait for the file to download
-        WebDriverWait(driver, 30).until(lambda d: os.path.exists(planning_ics_path))
-        print("Downloaded the planning.ics file.")
-        
-        """
-        # Read the content of the downloaded file
-        with open(planning_ics_path, "r") as file:
-            print("Content of the downloaded file:")
-            print(file.read())
-        """
-        
-        # Click 5: Next month
-        next_month_locator = (By.XPATH, "//button[@class='fc-next-button ui-button ui-state-default ui-corner-left ui-corner-right']")
-        WebDriverWait(driver, 30).until(EC.element_to_be_clickable(next_month_locator))
-        next_month_button = driver.find_element(By.XPATH, "//button[@class='fc-next-button ui-button ui-state-default ui-corner-left ui-corner-right']")
-        next_month_button.click()
-        print("Navigated to the next month.")
-        
-        
+            # Scroll to the bottom of the page to ensure the download icon is visible
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            print("Scrolled to the bottom of the page.")
+
+            # Delete planning.ics in downloads folder if it already exists
+            planning_ics_path = os.path.join(DOWNLOADS_PATH, "planning.ics")
+            if os.path.exists(planning_ics_path):
+                os.remove(planning_ics_path)
+                print("Deleted older planning.ics file.")        
+
+            # Click 4: Download the planning.ics file
+            download_icon_locator = (By.XPATH, "//i[contains(@class, 'fa fa-download Fs20 Gray')]")
+            WebDriverWait(driver, 30).until(EC.visibility_of_element_located(download_icon_locator))
+            print("Download icon is now visible.")
+
+            # Locate and click the download icon using JavaScript
+            download_icon = driver.find_element(By.XPATH, "//i[contains(@class, 'fa fa-download Fs20 Gray')]")
+            driver.execute_script("arguments[0].click();", download_icon)
+            print("Clicked on the download icon using JavaScript.")
+
+            # Wait for the file to download
+            WebDriverWait(driver, 30).until(lambda d: os.path.exists(planning_ics_path))
+            print("Downloaded the planning.ics file.")
+
+            # Scroll back to the top of the page to ensure "Next Month" button is clickable
+            driver.execute_script("window.scrollTo(0, 0);")
+            print("Scrolled back to the top of the page.")
+
+            # Click 5: Next month
+            next_month_locator = (By.XPATH, "//button[@class='fc-next-button ui-button ui-state-default ui-corner-left ui-corner-right']")
+            WebDriverWait(driver, 30).until(EC.element_to_be_clickable(next_month_locator))
+            next_month_button = driver.find_element(By.XPATH, "//button[@class='fc-next-button ui-button ui-state-default ui-corner-left ui-corner-right']")
+            next_month_button.click()
+            print("Navigated to the next month.")
+
 
     finally:
         input("Press Enter to close.")
